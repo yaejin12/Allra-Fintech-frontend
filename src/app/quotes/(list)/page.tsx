96d1342c434c4e9e0b5b8a991384e221cb6c24d3
yaejin12 <pos03167@naver.com>
2024-11-15 01:29:12 +0900

    🐛 이중 호출 버그 수정

--- src/app/quotes/(list)/page.tsx
+++ src/app/quotes/(list)/page.tsx
@@ -5,6 +5,9 @@ import { QuoteCard } from '@/app/quotes/components/quote-card'
 import { useInView } from 'react-intersection-observer'
 import { useEffect } from 'react'
 
+import { useRef } from 'react'
+import useThrottle from '../hooks/use-throttle'
+
 /**
  *
  * @returns 무한 스크롤로 Quotes 목록을 보여주는 페이지 컴포넌트
@@ -22,14 +25,17 @@ export default function QuotesPage() {
 
   // // Intersection Observer 훅을 사용
   const { ref, inView } = useInView({
-    threshold: 0.8,
+    threshold: 0.9,
   })
+
+  const fetchNextPageThrottle = useThrottle(fetchNextPage, 1000)
 
   useEffect(() => {
     if (inView && !isFetching && hasNextPage) {
-      fetchNextPage()
+      fetchNextPageThrottle()
+      console.log('inView', inView)
     }
-  }, [inView, hasNextPage, fetchNextPage, isFetching])
+  }, [inView])
 
   if (isLoading) {
     return <div>loading</div>
@@ -59,7 +65,7 @@ export default function QuotesPage() {
             />
           )
         })}
-      <div ref={ref} style={{ width: '100%', height: '200px' }}></div>
+      <div ref={ref} style={{ width: '100%', height: '80px' }}></div>
     </>
   )
 }
